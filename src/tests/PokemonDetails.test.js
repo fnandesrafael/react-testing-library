761acb3d24a597e79f757f66b01a1578f8c1c0e5
Rafael de Lima <rafaelima.fernandez@gmail.com>
2022-03-01 17:44:37 -0300

    :bug: Refatoraçao Requisito 07

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -26,12 +26,18 @@ describe('07 - Testa o componente PokemonDetails.js', () => {
     userEvent.click(screen.getByRole('link', { name: /more details/i }));
     expect(screen.getByRole('heading', { name: /game locations of pikachu/i }))
       .toBeInTheDocument();
-    pokemons[0].foundAt.forEach(({ location, map }, index) => {
-      expect(screen.getByText(location)).toBeInTheDocument();
-      expect(screen.queryAllByTestId('location-map')[index]).toHaveAttribute('src', map);
-      expect(screen.queryAllByTestId('location-map')[index])
-        .toHaveAttribute('alt', 'Pikachu location');
-    });
+    /**
+     * Esta parte do código foi construida a partir do código do aluno Carlos Rosa
+     * Source: https://github.com/tryber/sd-017-project-react-testing-library/pull/7
+     */
+    // Na linha 34, são procurados todas as imagens que possuem o atributo alt Pikachu location, para assim conseguir encontrar apenas as imagens que se tratam das localizações.
+    const maps = screen.getAllByAltText('Pikachu location');
+    // nas linhas 37 e 38 são feitos os testes de expect, esperando que o atributo src sejam exatamente os do arquivo data.js
+    expect(maps[0].src).toBe('https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+    expect(maps[1].src).toBe('https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png');
+    // Nas linhas 40 e 41 acontecem os mesmos testes, mas apenas esperando que existam os nomes das localizações
+    expect(screen.getByText('Kanto Viridian Forest')).toBeInTheDocument();
+    expect(screen.getByText('Kanto Power Plant')).toBeInTheDocument();
   });
   it('Verifica se a pagina contém um checkbox para favoritar', () => {
     const { history } = renderWithRouter(<App />);
